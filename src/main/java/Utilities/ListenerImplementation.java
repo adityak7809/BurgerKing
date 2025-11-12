12c01720026192a9633d865b1a92922e36c07428
adityak7809 <adityak7809@gmail.com>
2025-11-12 13:58:41 +0530

    Changes in batch file(class name)

--- src/main/java/Utilities/ListenerImplementation.java
+++ src/main/java/Utilities/ListenerImplementation.java
@@ -1,34 +1,103 @@
 package Utilities;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.io.FileHandler;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class ListenerImplementation implements ITestListener{
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ListenerImplementation implements ITestListener, ISuiteListener{
 	
+	
+	public ExtentSparkReporter spark;
+	public ExtentReports report;
+	public static ExtentTest test;
+	String time=new Date().toString().replace(" ", "_").replace(":", "_");
+	String filePath="./AdvanceReport/Report_"+time+".html";
+
+
 	@Override
-	public void onTestFailure(ITestResult result)
-	{
-		String methodName = result.getMethod().getMethodName();
-		TakesScreenshot ts = (TakesScreenshot)BaseClass.sDriver;
+	public void onStart(ISuite suite) {
+		System.out.println("Report configuration");
 		
-		File src = ts.getScreenshotAs(OutputType.FILE);
-		File dstn = new File("./Screenshot/failedTest"+"_"+methodName+".png");
 		
-//		src.renameTo(dstn);
-	
 		
+		spark=new ExtentSparkReporter(filePath);
+		spark.config().setTheme(Theme.DARK);
+		spark.config().setDocumentTitle("CRM Test Suite Result");
+		spark.config().setReportName("CRM Test Report");
+		
+
+		report=new ExtentReports();
+		report.attachReporter(spark);
+		report.setSystemInfo("OS", "Windows-11");
+		report.setSystemInfo("BROWSER", "Chrome 1.123");
+
+	}
+
+	@Override
+	public void onFinish(ISuite suite) {
+		System.out.println("Report backup");
+
+		report.flush();
+		
+		File file=new File(filePath);
 		try {
-			FileHandler.copy(src, dstn);
+			Desktop.getDesktop().open(file);
 		} catch (IOException e) {
-		
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+       
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+	    test=report.createTest(result.getName()).assignAuthor("Aditya");
+	    UtilityClassObject.setTest(test);
+	    test.info(result.getName()+" ======>STARTED<======");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+	    test.info(result.getName()+" ======>COMPLETED<======");
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+	    test.fail(result.getName()+" ======>FAILED<======");
+
+		String testName=result.getName();
+
+		String time=new Date().toString().replace(" ", "_").replace(":", "_");
+
+		TakesScreenshot ts=(TakesScreenshot) BaseClass.sDriver;
+//		File src=ts.getScreenshotAs(OutputType.FILE);
+//		File tgt=new File("./Screenshot/"+testName+"_"+time+".png");
+//		src.renameTo(tgt);
+		
+		//Attach screenshot to Extent report
+		String file=ts.getScreenshotAs(OutputType.BASE64);
+		
+		test.addScreenCaptureFromBase64String(file, result.getName()+"_"+time);
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+
 	}
 	
 	
--- src/main/java/Utilities/UtilityClassObject.java
+++ src/main/java/Utilities/UtilityClassObject.java
@@ -1,18 +1,31 @@
 package Utilities;
 
+import com.aventstack.extentreports.ExtentTest;
+
 import io.appium.java_client.android.AndroidDriver;
 
 public class UtilityClassObject {
 
+	public static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 	public static ThreadLocal<AndroidDriver> driver = new ThreadLocal<AndroidDriver>();
-	
-	// getter driver
-		public static AndroidDriver getDriver() {
-			return driver.get();
-		}
 
-		// setter driver
-		public static void setDriver(AndroidDriver actDriver) {
-			driver.set(actDriver);
-		}
+	public static ExtentTest getTest()
+	{
+		return test.get();
+	}
+
+	public static void setTest(ExtentTest actTest)
+	{
+		test.set(actTest);
+	}
+
+	// getter driver
+	public static AndroidDriver getDriver() {
+		return driver.get();
+	}
+
+	// setter driver
+	public static void setDriver(AndroidDriver actDriver) {
+		driver.set(actDriver);
+	}
 }
